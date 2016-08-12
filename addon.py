4bc3be01d15168106f9c47c2b0399c5d6e82c635
Su Wang <wangsu10@gmail.com>
2016-08-12 09:29:28 -0400

    Update addon.py
    add wallop_port and channel selection (hd | favorites | all)

--- addon.py
+++ addon.py
@@ -19,16 +19,16 @@ def addDirectoryItem(parameters, li):
     return xbmcplugin.addDirectoryItem(handle=handle, url=url, 
         listitem=li, isFolder=True)
 
-def show_channels(wallop_server):
+def show_channels(wallop_server,wallop_port,channel_selection):
     
     # Check if Wallop Server has been defined
     addon = xbmcaddon.Addon('plugin.video.wallop')
-    if wallop_server == '':
+    if wallop_server == '' or wallop_port == '' or channel_selection == '':
         addon.openSettings()
         
     # Fetch Channel Lineup
-    conn = httplib.HTTPConnection(wallop_server, '8888', timeout=15)
-    url = '/channels?type=favorites'
+    conn = httplib.HTTPConnection(wallop_server, wallop_port, timeout=15)
+    url = "/channels?type=%s" % channel_selection
     headers = {"Accept":"application/json"}
     conn.request("GET", url, '', headers)
     response = conn.getresponse()
@@ -41,14 +41,14 @@ def show_channels(wallop_server):
         addDirectoryItem({"channel": item['GuideNumber']}, liStyle)    
     xbmcplugin.endOfDirectory(handle=handle, succeeded=True)
 
-def play_channel(channel,wallop_server):
+def play_channel(channel,wallop_server,wallop_port):
     
     # Fetch resolution and bitrate from settings page
     resolution = xbmcplugin.getSetting(handle,'resolution')
     bitrate = xbmcplugin.getSetting(handle,'bitrate')
     
     # Tune Channel
-    conn = httplib.HTTPConnection(wallop_server, 8888, timeout=15)
+    conn = httplib.HTTPConnection(wallop_server, wallop_port, timeout=15)
     url = "/channels/%s/tune?resolution=%s&bitrate=%s" % (channel, resolution, bitrate)
     conn.request("POST", url)
     conn.close
@@ -56,7 +56,7 @@ def play_channel(channel,wallop_server):
     # Keep Checking status of tuning until it returns ready
     while True:
 
-        conn = httplib.HTTPConnection(wallop_server,'8888', timeout=15)
+        conn = httplib.HTTPConnection(wallop_server,wallop_port, timeout=15)
         url = "/channels/%s/status" % channel
         conn.request("GET", url)
         response = conn.getresponse()
@@ -68,16 +68,18 @@ def play_channel(channel,wallop_server):
         else:
             break
 
-        link = "http://%s:8888/channels/%s.m3u8" % (wallop_server, channel)
+        link = "http://%s:%s/channels/%s.m3u8" % (wallop_server, wallop_port, channel)
         xbmc.Player().play(link)
 
 handle = int(sys.argv[1])
 wallop_server = xbmcplugin.getSetting(handle,'wallop')
+wallop_port = xbmcplugin.getSetting(handle,'wallopport')
+channel_selection = xbmcplugin.getSetting(handle,'channelselection')
         
 params = parameters_string_to_dict(sys.argv[2])
 channel = str(params.get("channel", ""))
 
 if channel == "":
-    show_channels(wallop_server)
+    show_channels(wallop_server,wallop_port,channel_selection)
 else:
-    play_channel(channel, wallop_server)
+    play_channel(channel, wallop_server, wallop_port)
